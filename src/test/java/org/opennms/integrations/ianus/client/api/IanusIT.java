382ee32939c035219fc133293cd034eb06035d7c
Antonio Russo <rssntn67@yahoo.it>
2026-02-26 13:22:48 +0100

    Added tests for measurements

--- src/test/java/org/opennms/integrations/ianus/client/api/IanusIT.java
+++ src/test/java/org/opennms/integrations/ianus/client/api/IanusIT.java
@@ -4,93 +4,91 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 import org.opennms.integrations.ianus.client.handler.OpenNmsRestClient;
-import org.opennms.integrations.ianus.client.model.ResourceDTO;
+import org.opennms.integrations.ianus.client.model.QueryRequest;
+import org.opennms.integrations.ianus.client.model.QueryResponse;
 import org.opennms.integrations.ianus.client.model.ResourceDTOCollection;
+import org.opennms.integrations.ianus.client.model.Source;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.List;
+import java.time.Instant;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
  * Integration tests for ResourcesApi against a live OpenNMS instance.
- *
  * Run with:
  *   ./mvnw verify \
  *     -Dopennms.it.enabled=true \
- *     -Dopennms.base-url=http://localhost:8980/opennms/rest \
+ *     -Dopennms.base-url=<a href="http://localhost:8980/opennms/rest">...</a> \
  *     -Dopennms.username=admin \
  *     -Dopennms.password=admin \
- *     -Dopennms.it.resource.id=node[1].nodeSnmp[] \
- *     -Dopennms.it.node.criteria=1
  */
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 @EnabledIfSystemProperty(named = "opennms.it.enabled", matches = "true")
-class ResourcesApiIT {
+class IanusIT {
 
     @Autowired
     private OpenNmsRestClient restClient;
 
-    private ResourcesApi api;
+    private ResourcesApi rApi;
+    private MeasumentsApi mApi;
 
     @BeforeEach
     void setUp() {
-        api = new ResourcesApi(restClient);
+        rApi = new ResourcesApi(restClient);
+        mApi = new MeasumentsApi(restClient);
     }
 
     @Test
     void getResources_returnsNonNullCollection() {
-        ResourceDTOCollection result = api.getResources(null);
+        ResourceDTOCollection result = rApi.getResources(null);
         assertThat(result).isNotNull();
-        if (result.getObjects() != null) {
-            result.getObjects()
-                    .forEach(a -> a.getChildren()
-                            .getObjects()
-                            .forEach(c -> System.out.println(c.getId())));
-        }
+        assertThat(result.getObjects()).isNotNull();
+        result.getObjects().forEach( o -> o.getChildren().getObjects().forEach( c -> {
+            System.out.println("----------------");
+            System.out.println(c.getId());
+            System.out.println(c.getLabel());
+            System.out.println(c.getName());
+            System.out.println(c.getParentId());
+            System.out.println(c.getLink());
+            System.out.println(c.getTypeLabel());
+        }));
     }
 
     @Test
-    void getResources_withDepth_returnsNonNullCollection() {
-        ResourceDTOCollection result = api.getResources(1);
-        assertThat(result).isNotNull();
+    void getMeasurement() {
+        String resourceId = "node[Home:55].responseTime[10.99.77.42]";
+        var end= Instant.now().toEpochMilli();
+        var start= end-600000;
+        System.out.println(start);
+        System.out.println(end);
+        System.out.println(-start+end);
+        /*
+        QueryRequest queryRequest = new QueryRequest();
+
+            queryRequest.setStart(start);
+            queryRequest.setEnd(end);
+            queryRequest.setStep(300L);
+            queryRequest.setMaxRows(100);
+            Source source = new Source();
+            source.setResourceId(resourceId);
+            source.setAggregation("AVERAGE");
+            source.setAttribute("icmp");
+            source.setLabel("icmp");
+            source.setTransient(true);
+
+            queryRequest.addSourcesItem(source);
+            QueryResponse queryResponse =mApi.query(queryRequest);
+            System.out.println("-----QueryResponse------");
+            System.out.println(queryResponse);
+*/
+        QueryResponse    queryResponse = mApi.simpleQuery(resourceId, "icmp", null, null, null, null,null, null, null);
+        System.out.println("-----QueryResponse------");
+        System.out.println(queryResponse);
+
     }
 
-    @Test
-    @EnabledIfSystemProperty(named = "opennms.it.resource.id", matches = ".+")
-    void getResourceById_returnsResourceWithMatchingId() {
-        String resourceId = System.getProperty("opennms.it.resource.id");
 
-        ResourceDTO result = api.getResourceById(resourceId, null);
 
-        assertThat(result).isNotNull();
-        assertThat(result.getId()).isEqualTo(resourceId);
-    }
-
-    @Test
-    @EnabledIfSystemProperty(named = "opennms.it.node.criteria", matches = ".+")
-    void getResourceForNode_returnsNonNullResource() {
-        String nodeCriteria = System.getProperty("opennms.it.node.criteria");
-
-        ResourceDTO result = api.getResourceForNode(nodeCriteria, null);
-
-        assertThat(result).isNotNull();
-    }
-
-    @Test
-    void select_withNoParams_returnsNonNullList() {
-        List<ResourceDTO> result = api.select(null, null, null, null);
-        assertThat(result).isNotNull();
-    }
-
-    @Test
-    @EnabledIfSystemProperty(named = "opennms.it.node.criteria", matches = ".+")
-    void select_withNodeFilter_returnsMatchingResources() {
-        String nodeCriteria = System.getProperty("opennms.it.node.criteria");
-
-        List<ResourceDTO> result = api.select(nodeCriteria, null, null, null);
-
-        assertThat(result).isNotNull();
-    }
 }
